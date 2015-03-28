e42e06e3c7296dc9647a531f433172e50b04d127
Thomas Kajder <tkajder@asu.edu>
2015-03-28 13:10:48 -0600

    Moved api key to environment variable. Added gzipping to api requests.

--- apicall.py
+++ apicall.py
@@ -1,18 +1,30 @@
 import json
 import requests
+import sys
 from pprint import pprint
-from config import API_KEY
+from config import API_KEY_ENV_VAR, API_URL, USER_AGENT
 
-REST_API = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key={}'
+def read_api_key():
+	api_key = os.environ.get(API_KEY_ENV_VAR)
+	if not api_key:
+		print('Please set the api key as the environment variable '.format(API_KEY_ENV_VAR))
+		sys.exit(1)
+	return api_key
 
 if __name__ == '__main__':
+	# Read the api key
+	api_key = read_api_key()
+
 	# Load the api request from file
 	with open('apirequest.json', 'r') as file:
 		json_request = json.load(file)
 
+	# Set gzip encoding headers
+	headers = {'Accept-Encoding:': 'gzip', 'User-Agent:': USER_AGENT}
+
 	# Request the data from the server
-	response = requests.post(REST_API.format(API_KEY), json=json_request)
-	
+	response = requests.post(API_URL.format(api_key), json=json_request, headers=headers)
+
 	# Retrieve the json response
 	if response.status_code == requests.codes.ok:
 		json_response = response.json()
---
+++ config.py
@@ -0,0 +1,3 @@
+API_URL = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key={}'
+API_KEY = 'AIzaSyAKRjJGdoI8XX6I3r2N_n07gLdWwzcF-_k'
+USER_AGENT = 'fuzzy-trip-planningv0.1 (gzip)'
