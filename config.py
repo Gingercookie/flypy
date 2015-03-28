b50b51e50ed462f3712a4cb5391198d169d80964
Will Graham <wgraham800@gmail.com>
2015-03-28 12:45:31 -0700

    Added FIELDS

--- config.py
+++ config.py
@@ -1,3 +1,19 @@
+FIELDS = {
+	'arrivalDate': 			'trips/tripOption/slice/segment/leg/arrivalTime',
+	'departureDate': 		'trips/tripOption/slice/segment/leg/departureTime',
+	'from': 				'trips/tripOption/slice/segment/leg/origin',
+	'to':	 				'trips/tripOption/slice/segment/leg/destination',
+	'duration':	 			'trips/tripOption/slice/segment/leg/duration',
+	'originTerminal':		'trips/tripOption/slice/segment/leg/originTerminal',
+	'destinationTerminal':	'trips/tripOption/slice/segment/leg/destinationTerminal',
+	'mileage':				'trips/tripOption/slice/segment/leg/mileage',
+	'layoverDuration':		'trips/tripOption/slice/segment/leg/connectionDuration',
+	'changePlane':			'trips/tripOption/slice/segment/leg/changePlane',
+	'baggage':				'trips/tripOption/pricing/segmentPricing/freeBaggageOption/pieces',
+	'price': 				'trips/tripOption/pricing/saleTotal',
+	'refundable':			'trips/tripOption/pricing/refundable'
+}
+
 API_URL = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key={}'
 API_KEY_ENV_VAR = 'FLYPY_APIKEY'
 USER_AGENT = 'fuzzy-trip-planningv0.1 (gzip)'
