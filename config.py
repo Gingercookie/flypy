a43db16898ba4c4534a0557c8ee691f3ad1ee09a
Will Graham <wgraham800@gmail.com>
2015-03-28 12:48:12 -0700

    Removed whatwewant.txt and updated config.py to reflect USER_AGENT of project

--- config.py
+++ config.py
@@ -16,4 +16,4 @@ FIELDS = {
 
 API_URL = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key={}'
 API_KEY_ENV_VAR = 'FLYPY_APIKEY'
-USER_AGENT = 'fuzzy-trip-planningv0.1 (gzip)'
+USER_AGENT = 'flypy_v0.1 (gzip)'
