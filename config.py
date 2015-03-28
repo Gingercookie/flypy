53af46bb8a9ffe1957ad528be6aad08d1b7c1378
Thomas Kajder <tkajder@asu.edu>
2015-03-28 13:22:32 -0600

    Commit correct config and add import os.

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 API_URL = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key={}'
-API_KEY = 'AIzaSyAKRjJGdoI8XX6I3r2N_n07gLdWwzcF-_k'
+API_KEY_ENV_VAR = 'FLYPY_APIKEY'
 USER_AGENT = 'fuzzy-trip-planningv0.1 (gzip)'
